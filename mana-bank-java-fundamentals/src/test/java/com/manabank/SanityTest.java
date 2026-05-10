6d3953c163a737d426abe76d23fa8848e4612b9e
Lohith <poreddylohith9381@gmail.com>
2026-05-09 22:55:24 -0400

    Initial Project setup with JUnit and Maven

---
+++ mana-bank-java-fundamentals/src/test/java/com/manabank/SanityTest.java
@@ -0,0 +1,12 @@
+package com.manabank;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SanityTest {
+
+    @Test
+    void shouldPass() {
+        assertEquals(2, 1+1);
+    }
+}
